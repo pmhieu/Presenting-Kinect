875516cbed6ba30d6095af9aa894c0f62632fc62
pmhieu <hieu8111989@yahoo.co.uk>
2012-04-26 17:22:42 +0100

    gesture recognition

---
+++ ProcessGesture.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Kinect;
+using AppSpecific;
+
+namespace GestureRegcognition
+{
+    public class ProcessGesture
+    {
+        bool isBackGestureActive = false;
+        bool isForwardGestureActive = false;
+        bool isLeftGestureActive = false;
+        bool isRightGestureActive = false;
+        public void ProcessGesture(Joint head, Joint rightHand, Joint leftHand, Joint rightElbow)
+        {
+            if (rightHand.Position.X > head.Position.X + 0.45)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive)
+                {
+                    isRightGestureActive = true;
+                    DefaultListener.gestureOccurs(RightGesture);
+                }
+            }
+            else
+            {
+                isRightGestureActive = false;
+            }
+
+            if (leftHand.Position.X < head.Position.X - 0.45)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive)
+                {
+                    isLeftGestureActive = true;
+                    DefaultListener.gestureOccurs(LeftGesture);
+                }
+            }
+            else
+            {
+                isLeftGestureActive = false;
+            }
+
+            if (rightHand.Position.Z > head.Position.Z + 0.45)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive)
+                {
+                    isForwardGestureActive = true;
+                    DefaultListener.gestureOccurs(UpGesture);
+                }
+            }
+            else
+            {
+                isForwardGestureActive = false;
+            }
+
+            if (rightElbow.Position.Z < head.Position.Z - 0.25)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive)
+                {
+                    isBackGestureActive = true;
+                    DefaultListener.gestureOccurs(DownGesture);
+                }
+            }
+            else
+            {
+                isBackGestureActive = false;
+            }
+        }
+    }
+}
