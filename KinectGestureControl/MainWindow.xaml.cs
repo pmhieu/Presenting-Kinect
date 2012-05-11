c4ab5440a96534ded1ff3e87c968f823b5407a67
pmhieu <hieu8111989@yahoo.co.uk>
2012-05-11 16:04:59 +0100

    Update

---
+++ KinectGestureControl/MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Speech.Recognition;
+using System.Threading;
+using System.IO;
+using Microsoft.Speech.AudioFormat;
+using System.Diagnostics;
+using System.Windows.Threading;
+
+namespace KinectGestureControl
+{
+    public partial class MainWindow : Window
+    {
+        KinectSensor sensor;
+
+        byte[] colorBytes;
+        Skeleton[] skeletons;
+        
+        bool isCirclesVisible = true;
+
+        bool isForwardGestureActive = false;
+        bool isBackGestureActive = false;
+        bool isLeftGestureActive = false;
+        bool isRightGestureActive = false;
+        SolidColorBrush activeBrush = new SolidColorBrush(Colors.Green);
+        SolidColorBrush inactiveBrush = new SolidColorBrush(Colors.Red);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Runtime initialization is handled when the window is opened. When the window
+            //is closed, the runtime MUST be unitialized.
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            //Handle the content obtained from the video camera, once received.
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            sensor = KinectSensor.KinectSensors.FirstOrDefault();
+
+            if (sensor == null)
+            {
+                MessageBox.Show("This application requires a Kinect sensor.");
+                this.Close();
+            }
+            
+            sensor.Start();
+
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+            sensor.SkeletonStream.Enable();
+            sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(sensor_SkeletonFrameReady);
+
+            sensor.ElevationAngle = 0;
+
+        }
+
+        void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (var skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame == null)
+                    return;
+
+                if (skeletons == null ||
+                    skeletons.Length != skeletonFrame.SkeletonArrayLength)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                }
+
+                skeletonFrame.CopySkeletonDataTo(skeletons);
+
+                Skeleton closestSkeleton = (from s in skeletons
+                                            where s.TrackingState == SkeletonTrackingState.Tracked &&
+                                                  s.Joints[JointType.Head].TrackingState == JointTrackingState.Tracked
+                                            select s).OrderBy(s => s.Joints[JointType.Head].Position.Z)
+                                                    .FirstOrDefault();
+
+                if (closestSkeleton == null)
+                    return;
+
+                var head = closestSkeleton.Joints[JointType.Head];
+                var rightHand = closestSkeleton.Joints[JointType.HandRight];
+                var leftHand = closestSkeleton.Joints[JointType.HandLeft];
+                var rightElbow = closestSkeleton.Joints[JointType.ElbowRight];
+
+                if (head.TrackingState != JointTrackingState.Tracked ||
+                    rightHand.TrackingState != JointTrackingState.Tracked ||
+                    leftHand.TrackingState != JointTrackingState.Tracked ||
+                    rightElbow.TrackingState != JointTrackingState.Tracked)
+                {
+                    //Don't have a good read on the joints so we cannot process gestures
+                    return;
+                }
+
+                SetEllipsePosition(ellipseHead, head, false);
+                SetEllipsePosition(ellipseLeftHand, leftHand, isLeftGestureActive);
+                SetEllipsePosition(ellipseRightHand, rightHand, isForwardGestureActive);
+
+                ellipseHead.Visibility = System.Windows.Visibility.Visible;
+                ellipseLeftHand.Visibility = System.Windows.Visibility.Visible;
+                ellipseRightHand.Visibility = System.Windows.Visibility.Visible;
+
+                ProcessGesture(head, rightHand, leftHand, rightElbow);
+            }
+        }
+
+        private void ProcessGesture(Joint head, Joint rightHand, Joint leftHand,Joint rightElbow)
+        {
+            if (rightHand.Position.X > head.Position.X + 0.45)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive )
+                {
+                    isRightGestureActive = true;
+                    System.Windows.Forms.SendKeys.SendWait("{Right}");
+                }
+            }
+            else
+            {
+                isRightGestureActive = false;
+            }
+
+            if (leftHand.Position.X < head.Position.X - 0.45)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive )
+                {
+                    isLeftGestureActive = true;
+                    System.Windows.Forms.SendKeys.SendWait("{Left}");
+                }
+            }
+            else
+            {
+                isLeftGestureActive = false;
+            }
+            if (rightHand.Position.Z > head.Position.Z - 0.4)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive)
+                {
+                    isForwardGestureActive = true;
+                    System.Windows.Forms.SendKeys.SendWait("{Up}");
+                }
+            }
+            else
+            {
+                isForwardGestureActive = false;
+            }
+
+            if (rightElbow.Position.Z < head.Position.Z + 0.12)
+            {
+                if (!isBackGestureActive && !isForwardGestureActive && !isLeftGestureActive && !isRightGestureActive)
+                {
+                    isBackGestureActive = true;
+                    System.Windows.Forms.SendKeys.SendWait("{Down}");
+                }
+            }
+            else
+            {
+                isBackGestureActive = false;
+            }
+        }
+
+        //This method is used to position the ellipses on the canvas
+        //according to correct movements of the tracked joints.
+        private void SetEllipsePosition(Ellipse ellipse, Joint joint, bool isHighlighted)
+        {
+            var point = sensor.MapSkeletonPointToColor(joint.Position, sensor.ColorStream.Format);
+
+            if (isHighlighted)
+            {
+                ellipse.Width = 60;
+                ellipse.Height = 60;
+                ellipse.Fill = activeBrush;
+            }
+            else
+            {
+                ellipse.Width = 20;
+                ellipse.Height = 20;
+                ellipse.Fill = inactiveBrush;
+            }
+
+            Canvas.SetLeft(ellipse, point.X - ellipse.ActualWidth / 2);
+            Canvas.SetTop(ellipse, point.Y - ellipse.ActualHeight / 2);
+
+        }
+
+    }
+}
